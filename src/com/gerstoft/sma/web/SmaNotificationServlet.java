3d6b1fb8d2a1ee6afcbfd4bb9703c9a9b1e7e638
Philip Gerstoft <philipgerstoft@gmail.com>
2016-02-28 17:18:09 -0800

    Add mebane fabers

--- src/com/gerstoft/sma/web/SmaNotificationServlet.java
+++ src/com/gerstoft/sma/web/SmaNotificationServlet.java
@@ -42,7 +42,7 @@ public class SmaNotificationServlet extends DataSourceServlet {
 	// UWM - Russell 2000 2x
 	// DRN - US REIT 3x
 
-	private static final String[] PHILIP_SYMBOLS = "ALFA,SCHX,SCHA,SCHF,VWO,SCHZ,SCHP,SCHH,QQQ,RWO,USO,GLD,SLV,IEF,GSG,JNK,STIP,VNQI,BNDX,SCHV"
+	private static final String[] PHILIP_SYMBOLS = "ALFA,SCHX,SCHA,SCHF,VWO,SCHZ,SCHP,SCHH,QQQ,RWO,USO,GLD,SLV,IEF,GSG,JNK,STIP,VNQI,BNDX,SCHV,GMOM,VAMO,GVAL,FYLD,GAA,SYLD"
 			.split(",");
 
 	private static final String[] JUSTIN_SYMBOLS = "VMMXX, PFORX, WACPX, VMISX, VSISX, VIFSX, RERGX, MINHX"
